e78119253643afbfeabd09638935a35963c1e0fd
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-07-18 19:56:01 +1000

    react apps

--- src/Components/Today.js
+++ src/Components/Today.js
@@ -6,7 +6,7 @@ function Today(props){
     // const dayName;
     console.log(props)
     return (
-        <div>
+        <div id='today'>
             <h1>{props.city}</h1>
             <img src={props.data.iconUrl} alt='pic not found'></img>
             <p>{props.data.temp}</p>
